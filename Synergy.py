f9425bf7c37bf356b7fe55a6b4e6743d81c5e17c
Evgeniy Ivanov <irelion_synergy@mail.ru>
2023-11-14 08:47:55 +0300

    P-0001: fix, entering numbers into a line

--- Synergy.py
+++ Synergy.py
@@ -1,15 +1,14 @@
 print('Введите количество чисел (1 ≤ N ≤ 100000)')
 n = int(input('N = '))
+
 if n > 100_000:
     print('Превышен диапазон числел N')
 else:
     numbers = set()
-    for i in range(n):
-        number = int(input(f'Число #{i + 1} = '))
+    numbersString = input('Введите числа через пробел: ')
+    for number in list(map(int, numbersString.split(' '))):
         if number > 1_000_000_000:
-            print('Число больше 1_000_000_000')
+            print(f'Число {number} больше 1_000_000_000')
         else:
             numbers.add(number)
-        break
-
     print(f'Количество уникальных значение = {len(numbers)}')
